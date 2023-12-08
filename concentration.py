56b47884156d85b9e27d9407b0c7fa2de9226af4
matt clifford <matt.clifford@bristol.ac.uk>
2023-12-08 14:36:09 +0000

    start mcdiarmids and proper R

---
+++ concentration.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def McDiarmids(R, N, delta):
+    # McDiarmid's inequality - bound the distance between
+    #       expected value and empircal mean
+    pass
--- radius.py
+++ radius.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-def R_upper_bound(R_emp, R_sup, N, delta):
+
+def R_upper_bound(R_emp, R_sup, N, delta, two=False):
     # eq. 5
-    R_expt = R_emp + (2*(R_sup/np.sqrt(N))) * (2+(np.sqrt(2*np.log(1/delta))))
+    if two == True:
+        R_expt = R_emp + (2*(R_sup/np.sqrt(N))) * (2+(np.sqrt(2*np.log(1/delta))))
+    else:
+        R_expt = R_emp + ((R_sup/np.sqrt(N))) * (2+(np.sqrt(2*np.log(1/delta))))
     return R_expt
 
 def calc_emp_R(X_proj, mean_proj):
