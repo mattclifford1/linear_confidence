63fa3d61df7478b1ac6fc35047807bf4ef407c1c
Matt Clifford <matt.clifford@bristol.ac.uk>
2024-01-15 15:29:28 +0000

    graph for optim

--- optimise_contraint.py
+++ optimise_contraint.py
@@ -6,26 +6,39 @@ get delta2 from delta1 by minimising the contraint
 
 
 def get_init_deltas(contraint_func, data_info):
-    '''
-    contraint_func: to equal 0 with args: delta1, delta2, data_info
-    delta1: fixed delta1 value
-    '''
-    deltas_init = (np.random.uniform(), np.random.uniform())
+   '''
+   contraint_func: to equal 0 with args: delta1, delta2, data_info
+   delta1: fixed delta1 value
+   '''
+   deltas_init = (np.random.uniform(), np.random.uniform())
+   deltas_init = (1, np.random.uniform())
 
-    def loss_wrapper(deltas_init):
-        return np.abs(contraint_func(deltas_init[0], deltas_init[1], data_info))
 
-    res = minimize(loss_wrapper,
-                   deltas_init,
-                #    (data_info),
-                   #    method='SLSQP',
-                   bounds=Bounds([0, 0], [1, 1]),
-                #    jac=use_grad,  # use gradient
-                #    constraints=contrs
-                   )
+   def loss_wrapper(deltas_init):
+      return np.abs(contraint_func(deltas_init[0], deltas_init[1], data_info))
 
-    # print(res.x[0])
-    # print(res.x[1])
-    # print(contraint_func(res.x[0], res.x[1], data_info))
+   import matplotlib.pyplot as plt
+   d2 = np.linspace(0, 1, 100)
+   loss = np.abs(contraint_func(deltas_init[0], d2, data_info))
+   print(f'min: {np.min(loss)}')
+   plt.plot(d2, loss)
+   plt.show()
 
-    return res.x[0], res.x[1]
+   res = minimize(loss_wrapper,
+                  deltas_init,
+               #    (data_info),
+                  #    method='SLSQP',
+                  bounds=Bounds([0, 0], [1, 1]),
+               #    jac=use_grad,  # use gradient
+               #    constraints=contrs
+                  )
+   print(res.x[0], res.x[1])
+
+   print(contraint_func(res.x[0], res.x[1], data_info))
+
+   d2 = np.linspace(0, 1, 100)
+   loss = np.abs(contraint_func(deltas_init[0], d2, data_info))
+   print(f'min: {np.min(loss)}')
+   print('-----------')
+
+   return res.x[0], res.x[1]
